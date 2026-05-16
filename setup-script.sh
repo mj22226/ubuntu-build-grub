c50de8a393ae0779d8a05db98425a61aecb90186
mj22226 <mj8263788@gmail.com>
2026-05-16 15:51:01 -0300

    Add files via upload

--- setup-script.sh
+++ setup-script.sh
@@ -4,7 +4,7 @@
 	sed -i 's/#EXTRA_GROUPS=.*/EXTRA_GROUPS="video"/g' /etc/adduser.conf
 	sed -i 's/#ADD_EXTRA_GROUPS=.*/ADD_EXTRA_GROUPS=1/g' /etc/adduser.conf
 	echo -n "rootwait rw console=ttyS2,1500000 console=tty1 cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" > /etc/kernel/cmdline
-	echo -n " text plymouth.ignore-serial-consoles apparmor=1 security=apparmor" >> /etc/kernel/cmdline
+	echo -n " text plymouth.ignore-serial-consoles apparmor=1 security=apparmor apparmor=1 security=apparmor" >> /etc/kernel/cmdline
 	rm -f /var/lib/dbus/machine-id
 	true > /etc/machine-id
 	touch /var/log/syslog
